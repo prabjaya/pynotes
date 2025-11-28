54092c63cd132dfcbe1852895e7209809ba0b04c
Prabha <spprabha87@gmail.com>
2025-11-28 12:31:32 +0100

    partialword code added

---
+++ prefixword.py
@@ -0,0 +1,31 @@
+stringwords=["car","cat","apple","man"]
+partialword = 'c'
+
+result =[]
+for i in stringwords:
+    if i.startswith(partialword):
+        result.append(i)
+
+print(result)
+
+
+def partialwordidentify(stringwards,partialword):
+    res = []
+    for i in stringwards:
+        if i.startswith(partialword):
+            res.append(i)
+
+    return res
+
+stringwords=["car","cat","apple","man","carret","carmodel"]
+partialword = 'c'
+
+print(partialwordidentify(stringwords,partialword))
+
+
+resulttest = partialwordidentify(stringwords,partialword)
+assert len(resulttest) == 4
+
+
+
+
